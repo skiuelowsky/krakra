7a8cf4b2a4cf139eb1fd47d7d15a4bddb90c95ed
skiuelowsky <skiuelowskyyy@gmail.com>
2020-05-25 18:32:11 +0200

    Update knp.command.js

--- src/commands/knp.command.js
+++ src/commands/knp.command.js
@@ -5,7 +5,7 @@ module.exports = {
     run(msg, args) {
       const {Client, RichEmbed, GuildMember} = require("discord.js")
       const { author, guild, channel,} = msg
-      if (msg.content === ".knp")
+      if (msg.content === "-knp")
   {
     const embed = new RichEmbed()
   
@@ -17,7 +17,7 @@ module.exports = {
     
   }
   
-  if (msg.content === "knp kamien")
+  if (msg.content === "-knp kamien")
   {
     let losowa_liczba = 0;
     losowa_liczba = losowa_liczba = (Math.round (Math.random () * 2))
@@ -58,7 +58,7 @@ module.exports = {
     }
   }
   
-  if (msg.content === "knp papier")
+  if (msg.content === "-knp papier")
   {
     let losowa_liczba = 0;
     losowa_liczba = losowa_liczba = (Math.round (Math.random () * 2))
@@ -99,7 +99,7 @@ module.exports = {
     }
   }
   
-  if (msg.content === "knp nozyce")
+  if (msg.content === "-knp nozyce")
   {
     let losowa_liczba = 0;
     losowa_liczba = losowa_liczba = (Math.round (Math.random () * 2))
