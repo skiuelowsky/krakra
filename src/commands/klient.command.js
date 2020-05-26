94c20ca3ac4668ac67b8463b6d9f96706865e002
skiuelowsky <skiuelowskyyy@gmail.com>
2020-05-26 20:42:56 +0200

    Update klient.command.js

--- src/commands/klient.command.js
+++ src/commands/klient.command.js
@@ -6,37 +6,26 @@ module.exports = {
       const {Client, RichEmbed, GuildMember} = require("discord.js")
       const { author, guild, channel,} = msg
 
-
-  if (msg.member.hasPermission("MANAGE_ROLES"))
-  {
-
-  let hejnal = msg.member.roles.find(r => r.id === "664534018632712193")
-
-  let member = msg.mentions.users.first()
-    if (!member)
-    {
-      const embed = new RichEmbed()
-      .setTitle("Nie podałeś komu chcesz dać klienta!")
-     .setColor("RED")
-     channel.send(embed)
-  
-    }
-   else
-   {
-      msg.member.addRole(hejnal)
-     const embed = new RichEmbed()
-     .setTitle( `Użytkownik ${member} stał się klientem. Witamy!`)
-     .setColor("RED")
-      channel.send(embed)
-   }
-      }
-      else
+      if (msg.content.startsWith("-klient"))
+      {
+        if (msg.member.hasPermission("MANAGE_ROLES"))
         {
-         const embed = new RichEmbed()
-         .setTitle("Za cienki w uszach jesteś koleś.")
-         .setColor("RED")
-         channel.send(embed)
-        }
+         let member = msg.mentions.users.first()
+         let clientrole = msg.member.roles.find(r => r.id === "664534018632712193")
+          msg.member.addRole(clientrole)
+          const embed = new RichEmbed()
+           .setTitle( `Użytkownik ${member} stał się klientem. Witamy!`)
+          .setColor("RED")
+              channel.send(embed)
+          }
+           else
+          {
+             const embed = new RichEmbed()
+             .setTitle("Za cienki w uszach jesteś koleś.")
+           .setColor("RED")
+           channel.send(embed)
+         }
+    }
       
     },
 }
