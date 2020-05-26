6f3cbc172d1bdfe8a17ccb86750a199c5fc11977
skiuelowsky <skiuelowskyyy@gmail.com>
2020-05-26 21:53:00 +0200

    Update pomoc.command.js

--- src/commands/pomoc.command.js
+++ src/commands/pomoc.command.js
@@ -9,7 +9,10 @@ module.exports = {
         const embed = new RichEmbed()
         .setTitle("Kreatorzy serwerów")
         .setDescription("Aby złożyć zamówienie udaj się na kanał <#714484679096533043> a następnie wpisz -zamowienie pakiet \n\nAby przyjąć zamówienie wpisz -przyjmij (musisz posiadać rangę kreator!)")
-        .addField("Made by","Skiuel")
+        .addFields(
+            {name:"Made by", value:"Skiuel",inline: true},
+            {name:"For", value:`You ${msg.author.username}`,inline: true},
+        )
         .setColor("#4287f5")
         channel.send(embed)
     },
