90643fc0669258393c2cd5f8a11a0b2cc8f431e3
skiuelowsky <skiuelowskyyy@gmail.com>
2020-05-26 21:35:43 +0200

    Create pomoc.command.js

---
+++ src/commands/pomoc.command.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: "pomoc",
+    description: "komenda ktora pokazuje komendy itp",
+  
+    run(msg, args) {
+        const {Client, RichEmbed, GuildMember} = require("discord.js")
+        const { author, guild, channel,} = msg
+
+        const embed = new RichEmbed()
+        .setTitle("Kreatorzy serwerów")
+        .setDescription("Aby złożyć zamówienie udaj się na kanał <#714484679096533043> a następnie wpisz -zamowienie pakiet \nAby przyjąć zamówienie wpisz -przyjmij (musisz posiadać rangę kreator!)")
+        .addField("Made by Skiuel for Kreatorzy Serwerów")
+        channel.send(embed)
+    },
+}
