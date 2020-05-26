9b23807b6a325b333a5816558b06e21559048a92
skiuelowsky <skiuelowskyyy@gmail.com>
2020-05-26 20:49:27 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -139,7 +139,26 @@ if (msg.content === "-zamknij")
   }
 }
     
-
+if (msg.content.startsWith("-klient"))
+{
+  if (msg.member.hasPermission("MANAGE_ROLES"))
+  {
+      let member = msg.mentions.users.first()
+      let clientrole = msg.member.roles.find(r => r.id === "664534018632712193")
+      msg.member.addRole(clientrole)
+      const embed = new RichEmbed()
+      .setTitle( `Użytkownik ${member.username} stał się klientem. Witamy!`)
+      .setColor("RED")
+      channel.send(embed)
+    }
+     else
+    {
+     const embed = new RichEmbed()
+     .setTitle("Za cienki w uszach jesteś koleś.")
+     .setColor("RED")
+     channel.send(embed)
+   }
+ }
 
         });
 
