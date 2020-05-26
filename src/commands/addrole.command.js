36730b8e545edc72b303cfafbb6653ab1592fc53
skiuelowsky <skiuelowskyyy@gmail.com>
2020-05-26 19:54:18 +0200

    Create addrole.command.js

---
+++ src/commands/addrole.command.js
@@ -0,0 +1,36 @@
+module.exports = {
+    name: "addrole",
+    description: "Dodaje dana role danemu uzytkownikowi",
+  
+    run(msg, args) {
+      const {Client, RichEmbed, GuildMember} = require("discord.js")
+      const { author, guild, channel,} = msg
+      function sleep (time) {
+        return new Promise((resolve) => setTimeout(resolve, time));
+      }
+      if (msg.content.startsWith("-addrole"))
+      {
+        if (msg.member.hasPermission("MANAGE_ROLES"))
+        {
+        msg.delete()
+        let member = msg.mentions.users.first()
+        let hejnal2 = msg.mentions.roles.first()
+ 
+        
+        msg.member.addRole(hejnal2)
+        const nie = new RichEmbed()
+        .setTitle("Użytkownikowi "+member.username+" dodano rolę "+hejnal2.name+"!")
+        .setColor("#02e63f")
+        channel.send(nie)
+        }
+        else
+        {
+          const nie = new RichEmbed()
+          .setTitle("Za cienki w uszach jesteś koleś.")
+          .setColor("0xfb0b0b")
+          channel.send(nie)
+          return
+        }
+      }
+    },
+}
