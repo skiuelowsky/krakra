257b0b99a2f087fcd435a20e48921566547dec27
skiuelowsky <skiuelowskyyy@gmail.com>
2020-05-27 22:04:50 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -59,7 +59,19 @@ client.on('GuildMemberAdd', member=>{
 
 
 
+client.on("message", async message =>
+{
+    
+    
 
+    const guild = message.guild;
+
+    const categoryChannels = guild.channels.filter(channel => channel.type === "category");
+
+    categoryChannels.forEach(channel => {
+        console.log(`Category ${channel.name} has ${channel.children.size} channels`);
+    });
+});
 
 //Weryfikacja
 client.on("message", (msg) => {
