ddd2705af5c8ac10d27a2b7ce350cca6d1271224
skiuelowsky <skiuelowskyyy@gmail.com>
2020-05-27 18:45:12 +0200

    Update pomoc.command.js

--- src/commands/pomoc.command.js
+++ src/commands/pomoc.command.js
@@ -7,8 +7,8 @@ module.exports = {
         const { author, guild, channel,} = msg
 
         const embed = new RichEmbed()
-        .setTitle("Kreatorzy serwerów")
-        .setDescription("**Zamówienia**\n\nAby złożyć zamówienie udaj się na kanał <#714484679096533043> a następnie wpisz -zamowienie pakiet \n\nAby przyjąć zamówienie wpisz -przyjmij (musisz posiadać rangę kreator!)\n\n**Fun**\n\n-iq - pokaże twoje iq.\n-zart - opowie ci śmieszny żart. \nmoneta - bot rzuci monetą. \nknp - gra kamień, papier, nożyce.\n")
+        .setTitle("Dream Discord")
+        .setDescription("**Zamówienia**\n\nAby złożyć zamówienie udaj się na kanał <#715240756587266212> a następnie wpisz .zamow tresc \n\nAby przyjąć zamówienie wpisz .przyjmij (musisz posiadać rangę maker!)\n\n**Fun**\n\n.iq - pokaże twoje iq.\n.zart - opowie ci śmieszny żart. \n.moneta - bot rzuci monetą. \n.knp - gra kamień, papier, nożyce.\n")
         .addField("Made by", "Skiuel", true)
         .addField("For", `You ${msg.author.username}❤`, true)
 
