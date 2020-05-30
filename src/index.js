8bd77072c6bf316d68bd0c0d5f725fdd19cce216
skiuelowsky <skiuelowskyyy@gmail.com>
2020-05-31 01:27:44 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -241,6 +241,10 @@ if (msg.content.startsWith(".klient"))
     if (msg.channel.name.startsWith("❗zamówienie-"))
     {
 
+      let przyjete = fs.readFileSync("przyjete.js", {encoding:"utf-8"})
+      przyjete = parseInt(przyjete)
+      przyjete = przyjete + 1
+      fs.writeFileSync("przyjete.js",przyjete)
 
       const name = msg.channel.name.split("❗zamówienie-")
       msg.channel.setName(`💚${name}-przyjęte`)
