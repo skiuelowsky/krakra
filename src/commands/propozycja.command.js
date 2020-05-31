b6e73b2bad6b28d5df696eb30f9c26929a959948
skiuelowsky <skiuelowskyyy@gmail.com>
2020-05-31 03:46:24 +0200

    Update propozycja.command.js

--- src/commands/propozycja.command.js
+++ src/commands/propozycja.command.js
@@ -4,7 +4,7 @@ module.exports = {
   
     run: async(msg, args)=> {
         const {Client, RichEmbed, GuildMember} = require("discord.js")
-        const { author, guild, channel,} = msg
+        const { author, guild, } = msg
         
         let wiadom = msg.content.split(".propozycja ")
 
@@ -17,9 +17,9 @@ module.exports = {
 
         const embed = new RichEmbed()
         .setDescription(wiadom)
-        const kox = guild.channels.find(channel => channel.id === "716438067895992422")
+        const channel = guild.channels.find(channel => channel.id === "716438067895992422")
 
-        let sendik = await msg.kox.send(embed)
+        let sendik = await msg.channel.send(embed)
         await sendik.react("✔")
         await sendik.react("❌")
 
