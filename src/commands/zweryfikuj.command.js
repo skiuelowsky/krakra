23c082e325088c67f3032543bcc6c6adb2c53b3e
skiuelowsky <skiuelowskyyy@gmail.com>
2020-06-01 01:28:40 +0200

    Create zweryfikuj.command.js

---
+++ src/commands/zweryfikuj.command.js
@@ -0,0 +1,36 @@
+module.exports = {
+    name: "zweryfikuj",
+    description: "weryfikuje uzytkownika",
+  
+    run(msg, args) {
+      const {Client, RichEmbed, GuildMember} = require("discord.js")
+      const { author, guild, channel,} = msg
+      if (msg.channel.id === "715134978068971556")
+      {
+        if (msg.content === ".zweryfikuj")
+        {
+            msg.delete()
+            const verifyrole = msg.guild.roles.find(r => r.id === "715134977859387438")
+            msg.member.addRole(verifyrole)
+            const verify = new RichEmbed()
+            .setTitle("Zostałeś poprawnie zweryfikowany!")
+            .setColor("GREEN")
+            msg.author.send(verify)
+        }
+        else
+        {
+            const embed = new RichEmbed()
+            .setTitle("Napisałeś coś innego niż **.zweryfikuj** spróbuj jeszcze raz.")
+            .setColor("RED")
+            msg.author.send(embed)
+        }
+      }
+      else
+      {
+        const embed = new RichEmbed()
+        .setTitle("Jesteś już zweryfikowany!")
+        .setColor("RED")
+        channel.send(embed)
+      }
+    },
+}
