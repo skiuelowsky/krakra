5ffc47548b59d2f332148d9f304bf88373c125c5
skiuelowsky <skiuelowskyyy@gmail.com>
2020-05-31 21:40:28 +0200

    Create reklama.command.js

---
+++ src/commands/reklama.command.js
@@ -0,0 +1,24 @@
+module.exports = {
+    name: "kick",
+    description: "Wyrzuca danego użytkownika",
+  
+    run(msg, args) {
+      const {Client, RichEmbed, GuildMember} = require("discord.js")
+      const { author, guild, channel,} = msg
+    
+      channel.send("``╔═════════════》Dream Discord《═════════════╗"+
+      +"**AUTORSKI BOT NAPISANY POD TWÓJ SERWER ZA DARMO.**"
+    
+      +"Na serwerze DreamDiscord jest promocja. "+
+      +"Wystarczy, że zaprosisz na serwer 2 osoby a będziesz mógł "+
+      +"otrzymać autorskiego"+
+      +"bota za darmo! Jak się zakwalifikować do promocji? Wszystkie s"+
+      +"szczegóły można zobaczyć na serwerze"+
+      +"Nasza strona: http://katosh.pl/"+
+      +"Invitelink:  https://discord.gg/hsEHcZY"+
+    +"╚═════════════》Dream Discord《═════════════╝``")
+
+
+
+    },
+}
