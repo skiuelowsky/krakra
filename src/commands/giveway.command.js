f7e9963135e90e72622fde5f0f5a5e7319f92ef0
skiuelowsky <skiuelowskyyy@gmail.com>
2020-05-31 03:26:33 +0200

    Update giveway.command.js

--- src/commands/giveway.command.js
+++ src/commands/giveway.command.js
@@ -6,8 +6,8 @@ module.exports = {
         const {Client, RichEmbed, GuildMember} = require("discord.js")
         const { author, guild, channel,} = msg
         
-        let msg = await msg.channel.send("elo")
-        await msg.react("💚")
+        let wiadom = await msg.channel.send("elo")
+        await wiadom.react("💚")
 
     },
 }
