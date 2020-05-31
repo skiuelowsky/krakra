18f40542deea36a90f9ba1ffe274b8b448b7c237
skiuelowsky <skiuelowskyyy@gmail.com>
2020-05-31 20:06:38 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -319,6 +319,36 @@ if (msg.content.startsWith("https://discord.gg/"))
   }
 }
 
+
+if (msg.channel.id === "715134978068971556")
+{
+  if (msg.content === ".zweryfikuj")
+  {
+      msg.delete()
+      const verifyrole = msg.guild.roles.find(r => r.id === "715134977859387438")
+      msg.member.addRole(verifyrole)
+      const verify = new RichEmbed()
+      .setTitle("Zostałeś poprawnie zweryfikowany!")
+      .setColor("GREEN")
+      msg.author.send(verify)
+  }
+  else
+  {
+      const embed = new RichEmbed()
+      .setTitle("Napisałeś coś innego niż **.zweryfikuj** spróbuj jeszcze raz.")
+      .setColor("RED")
+      msg.author.send(embed)
+  }
+}
+else
+{
+  const embed = new RichEmbed()
+  .setTitle("Jesteś już zweryfikowany!")
+  .setColor("RED")
+  channel.send(embed)
+}
+
+
         });
 
 
