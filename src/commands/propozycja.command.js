210c2ba390e68759656e07f27e1fb3681b6d5c98
skiuelowsky <skiuelowskyyy@gmail.com>
2020-05-31 03:39:09 +0200

    Create propozycja.command.js

---
+++ src/commands/propozycja.command.js
@@ -0,0 +1,27 @@
+module.exports = {
+    name: "propozycja",
+    description: "daje propozycje",
+  
+    run: async(msg, args)=> {
+        const {Client, RichEmbed, GuildMember} = require("discord.js")
+        const { author, guild, channel,} = msg
+        
+        let wiadom = msg.content.split(".propozycja ")
+
+        if (!wiadom)
+        {
+            channel.send("Oj, tak to nie wolno ! :(")
+            return
+        }
+
+
+        const embed = new RichEmbed()
+        .setDescription(wiadom)
+        let channel = guild.channels.find(channel => channel.id === "716438067895992422")
+
+        let sendik = await msg.channel.send(embed)
+        await sendik.react("✔")
+        await sendik.react("❌")
+
+    },
+}
