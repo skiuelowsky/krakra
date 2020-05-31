0dbe2c15a137697d8ccb98d4cfdad881275970fd
skiuelowsky <skiuelowskyyy@gmail.com>
2020-06-01 01:28:21 +0200

    Create klient.command.js

---
+++ src/commands/klient.command.js
@@ -0,0 +1,31 @@
+module.exports = {
+    name: "klient",
+    description: "daje klienta",
+  
+    run(msg, args) {
+      const {Client, RichEmbed, GuildMember} = require("discord.js")
+      const { author, guild, channel,} = msg
+
+      if (msg.content.startsWith("-klient"))
+      {
+        if (msg.member.hasPermission("MANAGE_ROLES"))
+        {
+            let member = msg.mentions.users.first()
+            let clientrole = msg.member.roles.find(r => r.id === "664534018632712193")
+            msg.member.addRole(clientrole)
+            const embed = new RichEmbed()
+            .setTitle( `Użytkownik ${member.username} stał się klientem. Witamy!`)
+            .setColor("RED")
+            channel.send(embed)
+          }
+           else
+          {
+           const embed = new RichEmbed()
+           .setTitle("Za cienki w uszach jesteś koleś.")
+           .setColor("RED")
+           channel.send(embed)
+         }
+       }
+      
+    },
+}
