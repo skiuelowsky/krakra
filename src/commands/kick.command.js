e934e24bd99f231f8cb33bfcede756d967d7d2de
skiuelowsky <skiuelowskyyy@gmail.com>
2020-05-31 14:56:23 +0200

    Update kick.command.js

--- src/commands/kick.command.js
+++ src/commands/kick.command.js
@@ -10,7 +10,7 @@ module.exports = {
        {
           const user = msg.mentions.users.first()
           const member = msg.guild.member(user)
-          let powod = msg.content.split(".kick ")
+          let powod = msg.content.slice(6)
           powod = powod - member
           if (!powod)
           {
