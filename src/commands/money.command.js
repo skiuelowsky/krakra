e8fb260573fb3e4988eaba5efbe88086e27ce97d
skiuelowsky <skiuelowskyyy@gmail.com>
2020-06-01 03:31:44 +0200

    Create money.command.js

---
+++ src/commands/money.command.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: "money",
+    description: "stan konta",
+  
+    run: async(msg, args)=> {
+        const {Client, RichEmbed, GuildMember} = require("discord.js")
+        const { author, guild, channel,} = msg
+        const Eco = require("quick.eco")
+        const eco = new Eco.Manager();
+
+        const kasa = fetchMoney(msg.author.id)
+        const embed = new RichEmbed()
+        .setDescription(`Twój stan konta wynosi:${kasa}`)
+        .setColor("#03fcb6")
+
+        
+
+    },
+}
