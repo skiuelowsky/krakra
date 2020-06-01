ce91c747b9fe265d2ce22c55bf35c901bc3194a9
skiuelowsky <skiuelowskyyy@gmail.com>
2020-06-01 02:51:22 +0200

    s

---
+++ src/commands/daily.command.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: "daily",
+    description: "sprawdza czy ktos jest gejem",
+  
+    run: async(msg, args)=> {
+        const {Client, RichEmbed, GuildMember} = require("discord.js")
+        const { author, guild, channel,} = msg
+        const Eco = require("quick.eco")
+        const eco = new Eco.Manager();
+
+        const embed = new RichEmbed()
+        .setDescription(`Odebrałeś już swój bonus! Spróbuj ponownie za ${add.time.hours}:${add.time.minutes}:${add.time.seconds}`)
+        .setColor("#03fcb6")
+
+        let add = eco.daily(message.author.id, 500)
+
+        if (add.onCooldown) return channel.send(embed)
+    },
+}
